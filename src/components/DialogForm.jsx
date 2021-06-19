9d4e0886f1e3d21c9f6ce305f615fb054a72274c
“spicecat” <ateh0701@gmail.com>
2021-06-18 23:06:52 -0500

    1

--- src/components/DialogForm.jsx
+++ src/components/DialogForm.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 import { upperFirst } from 'lodash/string'
+import { zipObject } from 'lodash/array'
+import { object, string } from 'yup'
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core'
 
 export default function DialogForm({ title, action, fields }) {
     const [open, setOpen] = useState(false)
     const toggleOpen = () => { setOpen(!open) }
 
-    const [form, setForm] = useState(fields.reduce((o, k) => (o[k] = '', o), {})) // remove and require form input
+    const [form, setForm] = useState(zipObject(fields)) // remove and require form input
+    const [errors, setErrors] = useState(zipObject(fields))
     const handleInputChange = e => {
         const { name, value } = e.target
         setForm({ ...form, [name]: value })
+        setErrors({ ...errors, [name]: !value })
     }
     const handleSubmit = async () => {
         const response = await action(form)
         console.log(response)
         if (response) {
-            setForm(fields.reduce((o, k) => (o[k] = '', o), {}))
+            setForm(zipObject(fields))
             toggleOpen()
         } else {
             console.log()
@@ -29,7 +33,7 @@ export default function DialogForm({ title, action, fields }) {
                 <DialogContent>
                     {fields.map(field =>
                         <div key={field}>
-                            <TextField name={field} label={upperFirst(field)} onChange={handleInputChange} required />
+                            <TextField name={field} label={upperFirst(field)} onChange={handleInputChange} required error={errors[field]} />
                         </div>
                     )}
                 </DialogContent>
