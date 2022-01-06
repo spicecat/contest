e9c1406db71be3d48a3d2d294f29f89192a9447b
spicecat <ateh0701@gmail.com>
2022-01-06 15:30:41 -0600

    dont remember

--- src/containers/Contest.jsx
+++ src/containers/Contest.jsx
@@ -2,21 +2,52 @@ import { useState, useEffect } from 'react'
 
 import { Table } from '../components'
 
-import { getContest } from '../services/contestService'
+import { getContest, getProblems } from '../services/contestService'
 
 function ContestTable({ contest = {} }) {
-  console.log(contest)
   return <Table data={[contest]} />
 }
+function showFile(blob) {
+  console.log(blob)
+  var newBlob = new Blob([blob], { type: "application/pdf" })
 
+  // IE doesn't allow using a blob object directly as link href
+  // instead it is necessary to use msSaveOrOpenBlob
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(newBlob);
+    return;
+  }
 
-export default function Contests() {
+  // For other browsers: 
+  // Create a link pointing to the ObjectURL containing the blob.
+  const data = window.URL.createObjectURL(newBlob);
+  var link = document.createElement('a');
+  link.href = data;
+  link.download = "file.pdf";
+  link.click();
+  setTimeout(function () {
+    // For Firefox it is necessary to delay revoking the ObjectURL
+    window.URL.revokeObjectURL(data);
+  }, 100);
+}
+
+export default function Contests(props) {
   const [contest, setContest] = useState()
-  const loadContest = async () => { setContest(await getContest('Test')) }
+  const [problems, setProblems] = useState()
+
+  const loadContest = async () => {
+    console.log(props)
+    setContest(await getContest('Test'))
+    setProblems(await getProblems('Test'))
+  }
 
   useEffect(() => { loadContest() }, [])
 
   return <>
     <ContestTable contest={contest} />
+    <object data="" type="application/pdf">
+      <div>No online PDF viewer installed</div>
+    </object>
+    <button onClick={() => showFile(problems)} />
   </>
 }
--- src/services/contestService.js
+++ src/services/contestService.js
@@ -7,15 +7,19 @@ const baseUrl = serverUrl + '/contest'
 export const getContests = async() => {
     try {
         const url = baseUrl
-        const { body: contests } = await superagent.get(url)
-        return contests
+        return (await superagent.get(url)).body
     } catch (err) {}
 }
 
 export const getContest = async(contest) => {
     try {
         const url = baseUrl
-        const { body: info } = await superagent.get(url).query({ contest })
-        return info
+        return (await superagent.get(url).query({ contest })).body
+    } catch (err) {}
+}
+export const getProblems = async(contest) => {
+    try {
+        const url = baseUrl + '/problems'
+        return (await superagent.get(url).query({ contest })).body.slice(2, -1)
     } catch (err) {}
 }
