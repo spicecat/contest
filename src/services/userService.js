ef6e73fdb1c7e38d806258258ca2f56a9c1656a2
Anthony Wang <ta180m@pm.me>
2022-01-17 22:06:11 -0600

    Implement federated login

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,34 @@
 import superagent from 'superagent'
 import Cookies from 'universal-cookie'
-import { url } from '../var.js'
 
 const cookies = new Cookies()
 
-export const register = async ({ username, name, email, password }) => {
+export const register = async ({ name, email, username, homeserver, password }) => {
     try {
-        const response = await superagent.post(url, {
+        const response = await superagent.post(homeserver, {
             type: 'register',
             name: name,
             email: email,
             username: username,
             password: password
         })
+        cookies.set('username', username)
+        cookies.set('homeserver', homeserver)
         cookies.set('password', password)
         return response.statusCode
     } catch ({ crossDomain, status }) { return crossDomain ? 521 : status }
 }
 
-export const login = async ({ username, password }) => {
+export const login = async ({ username, homeserver, password }) => {
     try {
-        console.log(await superagent.post(url, { type: 'about' }))
-        const response = await superagent.post(url, {
+        const response = await superagent.post(homeserver, {
             type: 'authenticate',
             username: username,
-            server: 'contest.exozy.me',
+            server: '',
             password: password
         })
+        cookies.set('username', username)
+        cookies.set('homeserver', homserver)
         cookies.set('password', password)
         return response.statusCode
     } catch ({ crossDomain, status }) { return crossDomain ? 521 : status }
@@ -34,6 +36,7 @@ export const login = async ({ username, password }) => {
 
 export const logout = () => {
     cookies.remove('username')
+    cookies.remove('homeserver')
     cookies.remove('password')
     window.location.href = '/'
 }
