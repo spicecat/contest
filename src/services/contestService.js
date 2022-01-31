a5c48cf2ab48c13cafbc6a277c8af95f7d2898cd
spicecat <ateh0701@gmail.com>
2022-01-31 12:20:54 -0600

    added contestContext

--- src/services/contestService.js
+++ src/services/contestService.js
@@ -1,22 +1,24 @@
 import superagent from 'superagent'
 import superagentCache from 'superagent-cache'
-import { url } from '../var.js'
 
 superagentCache(superagent, null, { preventDuplicateCalls: true })
 
-export const getContests = async ({ homeserver }) => {
+export const getContests = async({ homeserver }) => {
     try {
-        return (await superagent.post('https://' + homeserver)).body
-    } catch (err) { }
+        const { contests } = await superagent.post('https://' + homeserver, {
+            type: 'about'
+        })
+        return { contests }
+    } catch (err) {}
 }
 
-export const getContest = async ({ homeserver, contest }) => {
+export const getContest = async({ homeserver, contest }) => {
     try {
         return (await superagent.post('https://' + homeserver).query({ contest })).body
-    } catch (err) { }
+    } catch (err) {}
 }
-export const getProblems = async ({ homeserver, contest }) => {
+export const getProblems = async({ homeserver, contest }) => {
     try {
         return (await superagent.post('https://' + homeserver).query({ contest })).body.slice(2, -1)
-    } catch (err) { }
+    } catch (err) {}
 }
